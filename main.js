576066101281cd8e75cf545c7791c6768a3d21a4
aditya <adityakoda100@gmail.com>
2024-04-12 16:35:22 +0530

    commited

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const frequent = document.querySelectorAll('.freq');
 
 frequent.forEach(freq =>{
-    freq.addEventListener('click',() => {
+    freq.addEventListener('click',() => {   
         freq.classList.toggle('open');
     })
 })
+
+var myindex=0;
+slideshow();
+function slideshow(){
+    var x=document.getElementsByClassName("slide");
+    var i;
+    for(i=0;i<x.length;i++){
+        x[i].style.dislplay="none";
+
+    }
+    myindex++;
+    if(myindex>x.length){
+        myindex=1
+    }
+    x[myindex-1].style.display="block";
+    setTimeout(slideshow,3000);
+}
